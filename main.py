5244f3215fc4b76576dc5791eec49c58c015348b
masihgh <masihgraph@gmail.com>
2025-04-29 12:45:27 +0330

    append and some method for donators

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from app.config import get_login_credentials, LOGIN_URL
 from app.session_manager import load_session
 from app.coffeete import Coffeete
 
+
 def main():
     # Load session from file if available
     cookies = load_session()
@@ -16,9 +17,26 @@ def main():
 
     # Fetch and print payments
     payments = coffeete.get_all_payments()
+    for payment in payments:
+        print(payment)
+
+    # Fetch and print coffee data
     coffee_data = coffeete.get_coffee_and_today_donates()
-    print(payments)
-    print(coffee_data)
+    print(f"Today's Coffee: {coffee_data['today_coffee']}")
+    print(f"All Coffee: {coffee_data['all_coffee']}")
+
+    # Query the biggest coffee donation
+    biggest_coffee = coffeete.get_biggest_coffee_donate()
+    print(f"Biggest Coffee Donation: {biggest_coffee}")
+
+    # Query the latest coffee donation
+    latest_coffee = coffeete.get_latest_coffee_donate()
+    print(f"Latest Coffee Donation: {latest_coffee}")
+
+    # Query the 20 latest donations
+    latest_20_donates = coffeete.get_latest_20_donates()
+    print(f"Latest 20 Donations: {latest_20_donates}")
+
 
 if __name__ == "__main__":
     main()
